99c7b5e6e68a1c1d6a10a06906e41d26b73b7d34
lidebing <lidebing@chuchujie.com>
2017-06-22 18:02:59 +0800

    update css

--- dwxk/views.py
+++ dwxk/views.py
@@ -8,16 +8,16 @@ from . import models
 
 def index(request):
     items = models.ItemsInfo.objects.all()
-    category = [{u'c1': u'-1', u'name': u'女装'},
-                {u'c1': u'-2', u'name': u'女装'},
-                {u'c1': u'-3', u'name': u'女装'},
-                {u'c1': u'-4', u'name': u'女装'},
-                {u'c1': u'-5', u'name': u'女装'},
-                {u'c1': u'-6', u'name': u'女装'},
-                {u'c1': u'-7', u'name': u'女装'},
-                {u'c1': u'-8', u'name': u'女装'},
-                {u'c1': u'-9', u'name': u'女装'},
-                {u'c1': u'-10', u'name': u'女装'}
+    category = [{'c1': '-1', 'name': '女装'},
+                {'c1': '-2', 'name': '女装'},
+                {'c1': '-3', 'name': '女装'},
+                {'c1': '-4', 'name': '女装'},
+                {'c1': '-5', 'name': '女装'},
+                {'c1': '-6', 'name': '女装'},
+                {'c1': '-7', 'name': '女装'},
+                {'c1': '-8', 'name': '女装'},
+                {'c1': '-9', 'name': '女装'},
+                {'c1': '-10', 'name': '女装'}
                 ]
     return render(request, 'dwxk/index.html', {'items': items, 'category': category})
 
