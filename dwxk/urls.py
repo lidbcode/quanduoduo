ed87669a1ec3cc358dbf4561fbdeb28fc1990f93
lidb <lidbwork@163.com>
2017-06-23 01:51:35 +0800

    the frist commit

--- dwxk/urls.py
+++ dwxk/urls.py
@@ -18,6 +18,6 @@ from . import views
 
 urlpatterns = [
     url(r'^index/$', views.index),
-    url(r'^category\?cid=(?P<cid>-[0-9]+)&cname=(?P<cname>.+)$', views.get_category,name='dwxk'),
+    url(r'^category\?cid=(?P<cid>-[0-9]+)&cname=(?P<cname>.+)$', views.get_category, name='dwxk'),
     url(r'^$', views.test)
 ]
