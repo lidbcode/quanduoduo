89109d2b3b76e5592992f87c6cab98cd2aceaa6d
lidebing <lidebing@chuchujie.com>
2017-06-22 18:00:25 +0800

    update css

--- dwxk/views.py
+++ dwxk/views.py
@@ -8,8 +8,18 @@ from . import models
 
 def index(request):
     items = models.ItemsInfo.objects.all()
-    cname = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
-    return render(request, 'dwxk/index.html', {'items': items, 'cname': cname})
+    category = [{u'c1': u'-1', u'name': u'女装'},
+                {u'c1': u'-2', u'name': u'女装'},
+                {u'c1': u'-3', u'name': u'女装'},
+                {u'c1': u'-4', u'name': u'女装'},
+                {u'c1': u'-5', u'name': u'女装'},
+                {u'c1': u'-6', u'name': u'女装'},
+                {u'c1': u'-7', u'name': u'女装'},
+                {u'c1': u'-8', u'name': u'女装'},
+                {u'c1': u'-9', u'name': u'女装'},
+                {u'c1': u'-10', u'name': u'女装'}
+                ]
+    return render(request, 'dwxk/index.html', {'items': items, 'category': category})
 
 
 def test(requset):
