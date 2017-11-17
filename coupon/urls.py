1db524ba11fd779b93021a1af334b62166a75e09
lidebing <lidebing@chuchujie.com>
2017-11-17 19:10:21 +0800

    update

--- coupon/urls.py
+++ coupon/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from dwxk import views
+from dwxk import views as dwxk_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
+    url(r'^$', dwxk_views.index),
     url(r'^dwxk/', include('dwxk.urls', namespace='dwxk')),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
